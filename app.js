37eebe127a2d3e34a5f9c51cdec36fd82612b3f6
leonardo-monti <monti.leonardo22@gmail.com>
2026-03-01 20:44:14 +0100

    add menu list

--- app.js
+++ app.js
@@ -7,6 +7,39 @@ app.get("/", (req, res) => {
     res.json({ message: "benvenuto", })
 })
 
+app.get("/menu", (req, res) => {
+    const menu = [{
+        name: "Ciambellone",
+        image: "public/images/ciambellone.jpeg",
+        ingredients: ["farina , uova"],
+    },
+    {
+        name: "Cracker alla Barbabietola",
+        image: "public/images/cracker_barbabietola.jpeg",
+        ingredients: ["cracker , barbabietola"],
+    },
+    {
+        name: "Pane Fritto Dolce",
+        image: "public/images/pane_fritto_dolce.jpeg",
+        ingredients: ["pane , zucchero"],
+    },
+    {
+        name: "Pasta alla Barbabietola",
+        image: "public/images/pasta_barbabietola.jpeg",
+        ingredients: ["pasta , barbabietola"],
+    },
+    {
+        name: "Torta Paesana",
+        image: "public/images/torta_paesana.jpeg",
+        ingredients: ["farina , cioccolata"],
+    }]
+    const responseData = {
+        result: menu,
+        success: true,
+    }
+    res.json(menu)
+})
+
 app.listen(port, () => {
     console.log(`Server listening on ${appUrl}`)
 })
